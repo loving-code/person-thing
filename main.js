da05515f36d92f4412963a825cd4136819869028
loving-code <78284208+loving-code@users.noreply.github.com>
2021-11-27 19:01:31 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -11,9 +11,28 @@ Webcam.attach('#camera');
 
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-       document.getElementById("result").innerHTml = '<img id="captured_image" src='+data_uri+'"/>'
+       document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
 console.log('ml5 versions', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/81665RcJ7/', modelLoaded);
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/81665RcJ7/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded!')
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify (img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
